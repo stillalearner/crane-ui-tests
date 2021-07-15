3fcfd2531340160031c357f646ce9920ad01640d
nachandr <nachandr@redhat.com>
2021-07-15 09:15:56 -0500

    Add cluster view and page model

--- cypress/integration/models/cluster.ts
+++ cypress/integration/models/cluster.ts
@@ -1,4 +1,4 @@
-import { addCluster, button } from '../types/constants';
+//import { addCluster, button } from '../types/constants';
 import { clickByText, inputText, openSidebarMenu } from '../../utils/utils';
 import { navMenuPoint } from '../views/menu.view';
 import { ClusterData } from '../types/types';
@@ -6,27 +6,27 @@ import { clusterName, clusterUrl, instanceToken, addButtonModal } from '../views
 
 export class Cluster {
   protected static openLi(): void {
-    openSidebarMenu();
+    //openSidebarMenu();
     clickByText(navMenuPoint, 'Clusters');
   }
 
   protected openMenu(): void {
     //TODO: replace hardcoded timeout by expecting button to become clickable
-    cy.wait(2000);
+    //cy.wait(2000);
     Cluster.openLi();
   }
 
   protected runWizard(clusterData: ClusterData): void {
     const { name, url, token } = clusterData;
-    clickByText(button, addCluster);
+    clickByText('button', 'Add cluster');
     inputText(clusterName, name);
     inputText(clusterUrl, url);
     inputText(instanceToken, token);
-    clickByText(addButtonModal, addCluster);
+    clickByText(addButtonModal, 'Add cluster');
   }
 
   create(clusterData: ClusterData): void {
-    super.openMenu();
+    this.openMenu();
     this.runWizard(clusterData);
     //this.populate(providerData);
   }
--- cypress/integration/views/cluster.view.ts
+++ cypress/integration/views/cluster.view.ts
@@ -1,4 +1,4 @@
 export const clusterName = '#name';
 export const clusterUrl = '#url';
 export const instanceToken = '#token';
-export const addButtonModal = '#button.pf-c-button.pf-m-primary';
+export const addButtonModal = 'button.pf-c-button.pf-m-primary';
